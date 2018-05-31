20a3243672039d1688fe7407d9b8f5897947a707
Gilad Kutiel <gilad.kutiel@gmail.com>
2018-05-31 10:38:58 +0300

    publish

---
+++ publish.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+find docs/ -not -name CNAME -not -name docs -delete 
+parcel build --no-minify --out-dir docs index.html view/index.html edit/index.html
+git add -A
+git commit -m 'publish'
+git push
